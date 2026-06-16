56d355b9d0cab1bc0a45bb3cff1056bff7a2d71c
mrikardo <130250517+mrikardo@users.noreply.github.com>
2026-06-16 01:58:51 +0000

    Catalogo Videoke Rio

---
+++ script.js
@@ -0,0 +1,64 @@
+let musicas=[];
+
+fetch('musicas.json')
+.then(r=>r.json())
+.then(data=>{
+
+musicas=data;
+
+document.getElementById('contador')
+.innerHTML=
+`${musicas.length} músicas cadastradas`;
+
+});
+
+document
+.getElementById('busca')
+.addEventListener('input',pesquisar);
+
+function pesquisar(){
+
+const texto=
+document
+.getElementById('busca')
+.value
+.toLowerCase();
+
+const resultado=
+document
+.getElementById('resultado');
+
+resultado.innerHTML='';
+
+if(texto.length<1) return;
+
+const encontrados=
+musicas.filter(m=>
+
+m.codigo.toString().includes(texto) ||
+
+m.musica.toLowerCase().includes(texto) ||
+
+m.artista.toLowerCase().includes(texto)
+
+).slice(0,100);
+
+encontrados.forEach(m=>{
+
+resultado.innerHTML +=
+
+`<div class="card">
+
+<div class="codigo">
+${m.codigo}
+</div>
+
+<h3>${m.musica}</h3>
+
+<div>${m.artista}</div>
+
+</div>`;
+
+});
+
+}
